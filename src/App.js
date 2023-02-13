df0197d594ea9095cd4ac6f5b7a4a07c6974cdd0
Alek Novkovski <alek@greatcode2.com>
2023-02-13 22:45:43 +0100

    Transform received data into format this app wants

--- src/App.js
+++ src/App.js
@@ -27,6 +27,15 @@ function App() {
             })
             .then((data) => {
                 console.log(data)
+                const transformedMovies = data.results.map((movieData) => {
+                    return {
+                        id: movieData.episode_id,
+                        title: movieData.title,
+                        openingText: movieData.opening_crawl,
+                        releaseDate: movieData.release_date,
+                    };
+                });
+                console.log(transformedMovies)
             });
         setMovies(dummyMovies);
     }
